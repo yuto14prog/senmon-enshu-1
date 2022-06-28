a7200ce7b2792ddc24e9b1fe4c5695b83e37293b
Yuto <yuto14prog@gmail.com>
2022-06-28 12:18:44 +0900

    11

--- My_HTML.php
+++ My_HTML.php
@@ -2,9 +2,32 @@
 /* Page.php の内容を拡張する */
 include_once 'Page.php';
 class My_HTML extends Page {
+	function confirmPage() {
+		$body = '';
+		$body = $body . '<form method="post" action="index.php">';
+			$body = $body . '<input type="submit" name="page" value="修正"></input>';
+			$body = $body . '<input type="submit" name="page" value="確定"></input>';
+		$body = $body . '</form>';
+		return $body;
+	}
+
+	function modifyPage() {
+		$body = '';
+		$body = $body . '<form method="post" action="index.php">';
+			$body = $body . '<input type="submit" name="page" value="確認"></input>';
+		$body = $body . '</form>';
+		return $body;
+	}
+
+	function endPage() {
+		$body = '';
+		$body = $body . '<p>確定しました</p>';
+		return $body;
+	}
+
 	function inputPage(){
 		$body = '';
-		$body = $body . '<form method="post" action="answer.php">';
+		$body = $body . '<form method="post" action="index.php">';
 			$body = $body . '<input type="radio" name="q1" value="1">経済学<br>';
 			$body = $body . '<input type="radio" name="q1" value="2">経営学<br>';
 			$body = $body . '<input type="radio" name="q1" value="3">会計学<br><br>';
@@ -16,7 +39,7 @@ class My_HTML extends Page {
 			$body = $body . 'ID<input type="text" name="shimei"><br>';
 			$body = $body . 'パスワード<input type="password" name="password"><br><br>';
 
-			$body = $body . '<input type="submit"></input>';
+			$body = $body . '<input type="submit" name="page" value="回答"></input>';
 		$body = $body . '</form>';
 		return $body;
 	}
--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
 include_once  'My_HTML.php';
 
+// print_r($_POST);
 $page = new My_HTML();
-$page->setTitle('テスト');
 
-$body = $page->inputPage();
+if ( isset($_POST['page']) ) {
+  if ($_POST['page'] == '回答') {
+    $page->setTitle('確認');
+    $body = $page->confirmPage();
+    $page->setBody($body);
+  } else if ($_POST['page'] == '修正') {
+    $page->setTitle('修正');
+    $body = $page->modifyPage();
+    $page->setBody($body);
+  } else if ($_POST['page'] == '確認') {
+    $page->setTitle('確認');
+    $body = $page->confirmPage();
+    $page->setBody($body);
+  } else if ($_POST['page'] == '確定') {
+    $page->setTitle('確定');
+    $body = $page->endPage();
+    $page->setBody($body);
+  } else {
 
-$page->setBody($body);
+  }
+} else {
+  $page->setTitle('入力');
+  $body = $page->inputPage();
+  $page->setBody($body);
+}
 $page->display();
+
 ?>
