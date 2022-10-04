d577c76ebcb9a060fe9bd6344b7fd0fc6fe76f96
Yuto <yuto14prog@gmail.com>
2022-10-04 11:09:24 +0900

    end

--- Page.php
+++ Page.php
@@ -3,6 +3,30 @@ class Page {
 	private $title;
 	private $body;
 
+	function radioButton( $kv_array, $name, $init_val ) {
+		$button = '';
+		foreach ( $kv_array as $key => $value ) {
+			if ( $init_val == $key ) {
+				$button = $button.'<input type="radio" name="'.$name.'" value ="'.$key.'" checked >'.$value.'<br>';
+			} else {
+				$button = $button.'<input type="radio" name="'.$name.'" value ="'.$key.'">'.$value.'<br>';
+			}
+		}
+		return $button;
+	}
+
+	function checkBox( $kv_array, $name, $init_array ) {
+		$box = '';
+		foreach ( $kv_array as $key => $value ) {
+			if ( in_array( $key, $init_array ) ) {
+				$box = $box.'<input type="checkbox" name="'.$name.'[] " value ="'.$key.'" checked >'.$value.'<br>';
+			} else {
+				$box = $box.'<input type="checkbox" name="'.$name.'[] " value ="'.$key.'">'.$value.'<br>';
+			}
+		}
+		return $box;
+	}
+
 	function setTitle( $title ) {
 		$this->title = $title;
 	}
